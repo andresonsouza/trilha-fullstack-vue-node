f505131192b1d74ac4dc6e96ba0aa60621a687b2
Andreson Souza <aoliveiraso@minsait.com>
2025-08-30 00:07:58 -0300

    iniciando com css e melhorando rotas

--- item-04-vue-3/vue-sem-ia/src/App.vue
+++ item-04-vue-3/vue-sem-ia/src/App.vue
@@ -1,8 +1,50 @@
 <script setup></script>
 
-<template>
-  <h1>Hello World!</h1>
-  <router-view/>
-</template>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  background-color: var(--primary-color);
+}
 
-<style scoped></style>
+nav ul {
+  display: flex;
+  gap: 1rem;
+  list-style: none;
+}
+
+a {
+  color: #FFFFFF;
+  text-decoration: none;
+}
+
+nav ul li {
+  color: #FFFFFF;
+}
+
+.router-link-active {
+  color: #55efc4;
+}
+
+.logo a {
+  color: #FFFFFF;
+}
+
+</style>
+
+<template>
+  <header>
+    <div class="logo">
+      <router-link to="/">TECH STORE</router-link>
+    </div>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/produtos">Produtos</router-link></li>
+        <li><router-link to="/contato">Contato</router-link></li>
+      </ul>
+    </nav>
+  </header>
+  <router-view />
+</template>
